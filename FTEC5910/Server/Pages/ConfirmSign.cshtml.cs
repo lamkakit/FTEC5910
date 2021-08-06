129fdcb411faee24a2296cbe42349ed6e8c03889
lamkakit <49138575+lamkakit@users.noreply.github.com>
2021-08-07 01:01:15 +0800

    update confirm sign screen

--- FTEC5910/Server/Pages/ConfirmSign.cshtml.cs
+++ FTEC5910/Server/Pages/ConfirmSign.cshtml.cs
@@ -2,15 +2,90 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FTEC5910.Server.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace FTEC5910.Server.Pages
 {
+    [IgnoreAntiforgeryToken]
+
     public class ConfirmSignModel : PageModel
-    {
-        public void OnGet()
+    {        
+        [BindProperty]
+        public string IdentificationCode { get; set; }
+
+        [BindProperty]
+        public string PollId { get; set; }
+
+        public bool isError = false;
+        public string errorMsg = "";
+        public DataContext _db;
+
+        public string confirmLink = "";
+
+        public ConfirmSignModel(DataContext db) 
         {
+            _db = db;
+        }
+
+        public async Task OnGetAsync(string id, string code)
+        {
+            if (id == null)
+            {
+                isError = true;
+                errorMsg = "Missing id";
+            }
+            else 
+            {
+                try
+                {
+                    IdentificationCode = FTEC5910.Shared.Utilities.Decrypt(code);
+                }
+                catch (Exception ex)
+                {
+                    isError = true;
+                    errorMsg = "Invalid code";
+                    return;
+                }
+                try
+                {
+                    Guid guid;
+                    if (!Guid.TryParse(id, out guid))
+                    {
+                        throw new Exception("Wrong ID format!");
+                    }
+                    var poll = _db.PollingResults.Where(a => a.RequestID == guid && a.Type.Equals("Sign")).FirstOrDefault();
+                    if (poll != null && poll.Status == "Wait")
+                    {
+                        confirmLink = $"api/sign/confirmpoll?id={id}";
+                        PollId = id;
+                        return;
+                    }
+                    else
+                    {
+                        throw new Exception("Id not found or invalid status!");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    isError = true;
+                    if (ex.InnerException != null)
+                    {
+                        errorMsg = $"{ex.Message} {ex.InnerException.Message}";
+                    }
+                    else
+                    {
+                        errorMsg = $"{ex.Message}";
+                    }
+                }
+            }
+
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            return new OkObjectResult($"OK");
         }
     }
 }
--- FTEC5910/Shared/Utilities.cs
+++ FTEC5910/Shared/Utilities.cs
@@ -35,6 +35,16 @@ namespace FTEC5910.Shared
 
         }
 
+        public static int GenerateNumber(int length)
+        {
+            RandomNumberGenerator random = RandomNumberGenerator.Create();
+            var data = new byte[16];            
+            random.GetNonZeroBytes(data);
+            int temp = Math.Abs(BitConverter.ToInt32(data,0));
+            var powered = temp * Math.Pow(10, length);
+            return Convert.ToInt32(powered.ToString().Substring(0, length));
+        }
+
         public static string GetDescriptionFromEnum(Enum value)
         {
             if (value == null) return "";
@@ -49,17 +59,32 @@ namespace FTEC5910.Shared
         //https://github.com/kashifsoofi/crypto-sandbox/blob/master/dotnet/src/Sandbox.Crypto/AesBcCrypto.cs
         public static string Encrypt(string text) 
         {
-            string key = "ftec5920";
+            string key = "ftec5920ftec5920";
             byte[] iv = new byte[16];
             var keyParameters = CreateKeyParameters(Encoding.UTF8.GetBytes(key), iv);
-            var cipher = CipherUtilities.GetCipher("AES");
+            var cipher = CipherUtilities.GetCipher("AES/CBC/PKCS7");
             cipher.Init(true, keyParameters);
 
-            var plainTextData = Encoding.UTF8.GetBytes(text);
-            var cipherText = cipher.DoFinal(plainTextData);
+            var extra = GenerateNonce<byte[]>(12);
+            var plainTextData = Encoding.UTF8.GetBytes($"{text}").ToList();
+            plainTextData.AddRange(extra);
+            var cipherText = cipher.DoFinal(plainTextData.ToArray());
 
-            return Convert.ToBase64String(cipherText);
+            return Convert.ToBase64String(cipherText).Replace("+","-").Replace("/","_");
         }
+        public static string Decrypt(string cipherText)
+        {
+            string key = "ftec5920ftec5920";
+            byte[] iv = new byte[16];
+            var keyParameters = CreateKeyParameters(Encoding.UTF8.GetBytes(key), iv);
+            var cipher = CipherUtilities.GetCipher("AES/CBC/PKCS7");
+            cipher.Init(false, keyParameters);
+
+            var encryptedBytes = Convert.FromBase64String(cipherText.Replace("-", "+").Replace("_", "/"));
+            var decryptedData = cipher.DoFinal(encryptedBytes).SkipLast(12);
+            return Encoding.UTF8.GetString(decryptedData.ToArray()); ;
+        }
+
         private static ICipherParameters CreateKeyParameters(byte[] key, byte[] iv)
         {
             var keyParameter = new KeyParameter(key);
